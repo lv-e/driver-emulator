047baa1f6c943d3e31a6d577746e2eeefde3384b
Lino <lgustavo@enjoei.com.br>
2020-04-19 12:28:13 -0300

    up

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "now build for lv-emulator"
-make -f scripts/build/Makefile
+make -f /lv/scripts/build/Makefile
 
 echo "done! cleaning artifacts..."
-make -f scripts/build/Makefile clean
+make -f /lv/scripts/build/Makefile clean
 
 echo "there you go! look at /lv/bin/game"
 echo "have fun!"
